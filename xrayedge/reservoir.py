57beb62bd08f26f78591bdd78846d2338257f496
Corentin Bertrand <cbertrand@flatironinstitute.org>
2022-04-19 22:39:51 -0400

    Change interface reservoir

--- xrayedge/reservoir.py
+++ xrayedge/reservoir.py
@@ -5,27 +5,28 @@ from copy import copy
 
 
 class Reservoir:
-    def __init__(self, physics_params, accuracy_params):
+    def __init__(self):
         """
         Abstract class for a generic reservoir.
         """
-        self.PP = copy(physics_params)
-        self.AP = copy(accuracy_params)
-
         self.N = 3  # nr of different charge states affecting the QPC
 
         self._cache_g_less_t = [None] * self.N
         self._cache_g_grea_t = [None] * self.N
 
-    def g_less(self, w_array, Q):
+    def g_less_t(self, Q):
         """
-        Lesser GF in frequencies of contact site.
+        Lesser GF in times of contact site.
+
+        Returns times, values
         """
         raise NotImplementedError
 
-    def g_grea(self, w_array, Q):
+    def g_grea_t(self, Q):
         """
-        Greater GF in frequencies of contact site.
+        Greater GF in times of contact site.
+
+        Returns times, values
         """
         raise NotImplementedError
 
@@ -36,10 +37,7 @@ class Reservoir:
         Returns a (cached) function
         """
         if self._cache_g_less_t[Q] is None:
-            g_less = self.g_less(self.AP.omegas_fft(), Q=Q)
-
-            times, g_less_t = tb.inv_fourier_transform(self.AP.omegas_fft(), g_less)
-
+            times, g_less_t = self.g_less_t(Q)
             self._cache_g_less_t[Q] = interpolate.CubicSpline(times, g_less_t)
 
         return self._cache_g_less_t[Q]
@@ -51,10 +49,7 @@ class Reservoir:
         Returns a (cached) function
         """
         if self._cache_g_grea_t[Q] is None:
-            g_grea = self.g_grea(self.AP.omegas_fft(), Q=Q)
-
-            times, g_grea_t = tb.inv_fourier_transform(self.AP.omegas_fft(), g_grea)
-
+            times, g_grea_t = self.g_grea_t(Q)
             self._cache_g_grea_t[Q] = interpolate.CubicSpline(times, g_grea_t)
 
         return self._cache_g_grea_t[Q]
@@ -71,7 +66,9 @@ class QPC(Reservoir):
             physics_params -- a `PhysicsParameters` instance
             accuracy_params -- a `AccuracyParameters` instance
         """
-        super().__init__(physics_params, accuracy_params)
+        super().__init__()
+        self.PP = copy(physics_params)
+        self.AP = copy(accuracy_params)
 
     def delta_leads_R(self, w_array):
         """
@@ -127,3 +124,21 @@ class QPC(Reservoir):
             0.5 * self.delta_leads_K(w_array)
             + 1.0j * np.imag(self.delta_leads_R(w_array))
         )
+
+    def g_less_t(self, Q):
+        """
+        Lesser GF in times of contact site.
+        """
+        g_less = self.g_less(self.AP.omegas_fft(), Q=Q)
+
+        times, g_less_t = tb.inv_fourier_transform(self.AP.omegas_fft(), g_less)
+        return times, g_less_t
+
+    def g_grea_t(self, Q):
+        """
+        Greater GF in times of contact site.
+        """
+        g_grea = self.g_grea(self.AP.omegas_fft(), Q=Q)
+
+        times, g_grea_t = tb.inv_fourier_transform(self.AP.omegas_fft(), g_grea)
+        return times, g_grea_t
