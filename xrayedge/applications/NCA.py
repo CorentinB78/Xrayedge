6899c625a906de93e9d9f7d0c9f170364cfee3cf
Corentin Bertrand <cbertrand@flatironinstitute.org>
2022-04-20 00:33:57 -0400

    Remove weight in NCA app + rename methods

--- xrayedge/applications/NCA.py
+++ xrayedge/applications/NCA.py
@@ -3,6 +3,8 @@ from copy import copy
 from ..solver import PhysicsParameters, AccuracyParameters, CorrelatorSolver
 from ..reservoir import QPC
 
+# TODO: rename
+
 
 class NCASolver:
     """
@@ -23,21 +25,20 @@ class NCASolver:
             QPC(self.PP), self.PP.capac_inv, self.AP
         )
 
-    def G_grea_NCA_constraint(self, t_array):
+    def G_grea(self, t_array):
         """
-        Greater Green function in times on the QD under NCA constrain.
+        Pseudo-particle greater Green function in times on the QD
         """
         # no U in NCA constraint
         return (
             -1j
             * np.exp(-1j * t_array * self.PP.eps_d)
-            * self.weight(0, 0)
             * self.correlator_solver.A_plus(0, t_array)
         )
 
-    def G_reta_w_NCA_constraint(self, nr_freqs):
+    def G_reta_w(self, nr_freqs):
         """
-        Greater-retarded Green function in frequencies on the QD under NCA constrain.
+        Pseudo-particle greater-retarded Green function in frequencies on the QD.
 
         For NCA in the steady state regime, one only needs the greater quaisparticle GFs in the sector Q=0 (see notes).
         Also, the partition function is reduced to 1.
@@ -46,4 +47,4 @@ class NCASolver:
         """
         # no U in NCA constraint
         w, A_w, energy_shift = self.correlator_solver.A_plus_reta_w(0, nr_freqs)
-        return w, -1j * self.weight(0, 0) * A_w, energy_shift - self.PP.eps_d
+        return w, -1j * A_w, energy_shift - self.PP.eps_d
