c1a3709e23cb61716c99a7c5124804cb2229003e
Corentin Bertrand <cbertrand@flatironinstitute.org>
2022-05-13 12:55:21 -0400

    [NCA] add utility function

--- xrayedge/applications/NCA.py
+++ xrayedge/applications/NCA.py
@@ -1,5 +1,7 @@
 import numpy as np
+from matplotlib import pyplot as plt
 from copy import copy
+import toolbox as tb
 from ..solver import PhysicsParameters, AccuracyParameters, CorrelatorSolver
 from ..reservoir import QPC
 
@@ -47,3 +49,69 @@ class NCASolver:
         # no U in NCA constraint
         w, A_w, energy_shift = self.correlator_solver.A_plus_reta_w(0, nr_freqs)
         return w, -1j * A_w, energy_shift + self.PP.eps_QD
+
+
+def clean_and_interp_G_reta_w(w, wp, fp, wp_shift, tol=1e-3, plot=False):
+
+    wp_sh = wp + wp_shift
+
+    f_real = 1.0 / (w - wp_shift)
+    mask = np.abs(w - wp_shift) < wp[-1] / 10.0
+    f_real[mask] = np.interp(w[mask], wp_sh, fp.real)
+
+    mask_p = -fp.imag > tol
+
+    idx_left = np.nonzero(mask_p)[0][0]
+    idx_right = np.nonzero(mask_p)[0][-1]
+    mask_left = w <= wp_sh[mask_p][0]
+    mask_right = w >= wp_sh[mask_p][-1]
+    mask_center = ~np.logical_or(mask_left, mask_right)
+
+    f_imag = np.zeros_like(w)
+
+    slope = (np.log(-fp.imag[idx_left + 1]) - np.log(-fp.imag[idx_left])) / (
+        wp_sh[idx_left + 1] - wp_sh[idx_left]
+    )
+
+    if slope < 0.0:
+        print(f"XXX slope = {slope} is negative!")
+
+    intercept = np.log(-fp.imag[idx_left]) - slope * (wp_sh[idx_left])
+    f_imag[mask_left] = -np.exp(intercept + w[mask_left] * slope)
+
+    f_imag[mask_center] = np.interp(w[mask_center], wp_sh, fp.imag)
+
+    slope = (np.log(-fp.imag[idx_right]) - np.log(-fp.imag[idx_right - 1])) / (
+        np.log(wp[idx_right]) - np.log(wp[idx_right - 1])
+    )
+    intercept = np.log(-fp.imag[idx_right]) - slope * np.log(wp[idx_right])
+    f_imag[mask_right] = -np.exp(intercept + np.log(w[mask_right] - wp_shift) * slope)
+
+    f = f_real + 1j * f_imag
+
+    if plot:
+        plt.axhline(tol, c="k", ls=":")
+        plt.plot(w, -f.imag)
+        plt.plot(wp_sh, -fp.imag, "--")
+        plt.semilogy()
+        xcenter = (wp_sh[0] + wp_sh[-1]) / 2.0
+        xdev = np.abs(wp_sh[0] - wp_sh[-1])
+        plt.xlim(xcenter - xdev, xcenter + xdev)
+        tb.autoscale_y(logscale=True)
+        tb.ylim_max(tol * 1e-3, 1e10 * tol)
+        # plt.ylim(tol * 1e-3)
+        plt.show()
+
+        plt.axvline(wp_shift + wp[-1] / 10.0, c="k", ls=":")
+        plt.axvline(wp_shift - wp[-1] / 10.0, c="k", ls=":")
+        plt.plot(w, np.abs(f.real))
+        plt.plot(wp_sh, np.abs(fp.real), "--")
+        plt.semilogy()
+
+        plt.xlim(xcenter - xdev, xcenter + xdev)
+        tb.autoscale_y(logscale=True)
+        plt.show()
+
+    print("Norm F_reta_w:", -np.trapz(x=w, y=f) / np.pi, "== 1.0j ?")
+
+    return f
--- xrayedge/applications/__init__.py
+++ xrayedge/applications/__init__.py
@@ -1,3 +1,3 @@
 from ..solver import PhysicsParameters, AccuracyParameters, gen_params
 from .xray_edge import XRayEdgeSolver
-from .NCA import NCASolver
+from .NCA import NCASolver, clean_and_interp_G_reta_w
