d5920f17d5cad2c97618d77e40d75fc22dafad84
Corentin Bertrand <cbertrand@flatironinstitute.org>
2022-05-18 21:04:48 -0400

    [NCA] go back on interp method

--- xrayedge/applications/NCA.py
+++ xrayedge/applications/NCA.py
@@ -1,10 +1,11 @@
 import numpy as np
-from scipy import interpolate
+from scipy import interpolate, integrate
 from matplotlib import pyplot as plt
 from copy import copy
 import toolbox as tb
 from ..solver import PhysicsParameters, AccuracyParameters, CorrelatorSolver
 from ..reservoir import QPC
+import bisect
 
 # TODO: rename
 # TODO: test energy shifts
@@ -82,37 +83,76 @@ def checked_interp(x, xp, fp, rtol, atol, **kwargs):
     return f
 
 
-def interp_one_over_G_reta_w(w, wp, fp, wp_shift, rtol=1e-2, atol=1e-4, plot=False):
+def tangent_exp(x, y, x0=None, idx=None):
+    """
+    Return function of x of the form a.e^{bx}, so that it is tangent to y(x) at x=x0.
 
-    wp_sh = wp + wp_shift
+    Assumes x sorted and y real
+    """
+    if idx is None:
+        assert x0 is not None
+        idx = bisect.bisect(x, x0)
+    else:
+        assert x0 is None
+        x0 = x[idx]
 
-    mask_p = np.abs(wp) < wp[-1] / 10.0
+    if idx == 0:
+        raise ValueError("x0 should be > x[1]")
+    if idx == len(x) - 1:
+        raise ValueError("x0 should be < x[-2]")
+    f0 = y[idx]
 
-    idx_left = np.nonzero(mask_p)[0][0]
-    idx_right = np.nonzero(mask_p)[0][-1]
-    mask_left = w <= wp_sh[mask_p][0]
-    mask_right = w >= wp_sh[mask_p][-1]
-    mask_center = ~np.logical_or(mask_left, mask_right)
+    if abs(f0) < 1e-200:
+        # then we assume Df0 is also zero
+        return lambda t: 0.0
 
-    f_real = np.zeros_like(w)
+    Df0 = (y[idx] - y[idx - 1]) / (x[idx] - x[idx - 1])
 
-    slope = (np.log(fp.real[idx_right]) - np.log(fp.real[idx_right - 1])) / (
-        np.log(wp[idx_right]) - np.log(wp[idx_right - 1])
-    )
-    intercept = np.log(fp.real[idx_right]) - slope * np.log(wp[idx_right])
-    f_real[mask_right] = np.exp(intercept + np.log(w[mask_right] - wp_shift) * slope)
+    b = Df0 / f0
 
-    slope = (np.log(-fp.real[idx_left + 1]) - np.log(-fp.real[idx_left])) / (
-        np.log(-wp[idx_left + 1]) - np.log(-wp[idx_left])
-    )
+    return lambda t: f0 * np.exp(b * (t - x0))
 
-    intercept = np.log(-fp.real[idx_left]) - slope * np.log(-wp[idx_left])
-    f_real[mask_left] = -np.exp(intercept + np.log(-w[mask_left] + wp_shift) * slope)
 
-    f_real[mask_center] = checked_interp(
-        w[mask_center],
-        wp_sh,
-        fp.real,
+def tangent_power_law(x, y, x0=None, idx=None):
+    """
+    Return function of x of the form a.x^b, so that it is tangent to y(x) at x=x0.
+
+    Assumes x sorted and y real and y(x0) != 0
+    """
+    if idx is None:
+        assert x0 is not None
+        idx = bisect.bisect(x, x0)
+    else:
+        assert x0 is None
+        x0 = x[idx]
+
+    if idx < 1:
+        raise ValueError("x0 should be > x[1]")
+    if idx > len(x) - 2:
+        raise ValueError("x0 should be < x[-2]")
+    f0 = y[idx]
+    Df0 = (y[idx] - y[idx - 1]) / (x[idx] - x[idx - 1])
+
+    b = x0 * Df0 / f0
+
+    return lambda t: f0 * (t / x0) ** b
+
+
+def extrap_G_reta_w(x, xp, yp, tol, rtol=1e-2, atol=1e-4, plot=False):
+
+    ### real part
+    x0 = xp[-1] / 10.0
+    assert x0 > 0.0
+    f_real = np.empty_like(x)
+    mask = x > x0
+    f_real[mask] = tangent_power_law(xp, yp.real, x0=x0)(x[mask])
+    mask = x < -x0
+    f_real[mask] = tangent_power_law(xp, yp.real, x0=-x0)(x[mask])
+    mask = np.abs(x) <= x0
+    f_real[mask] = checked_interp(
+        x[mask],
+        xp,
+        yp.real,
         rtol=rtol,
         atol=atol,
         kind="cubic",
@@ -120,17 +160,41 @@ def interp_one_over_G_reta_w(w, wp, fp, wp_shift, rtol=1e-2, atol=1e-4, plot=Fal
         copy=False,
     )
 
-    delta = abs(wp[-1] - wp[0]) * 0.1
-    mask = np.logical_and(w >= wp_sh[0] + delta, w <= wp_sh[-1] - delta)
+    if plot:
+        plt.plot(x, f_real, "--")
+        plt.plot(xp, yp.real)
 
-    assert mask.any()
+        plt.loglog()
+        tb.autoscale_y(logscale=True)
 
-    f_imag = np.empty_like(f_real)
+        plt.axvline(x0, c="k", ls=":", alpha=0.4)
+        plt.show()
 
+        plt.plot(-x, -f_real, "--")
+        plt.plot(-xp, -yp.real)
+
+        plt.loglog()
+        tb.autoscale_y(logscale=True)
+
+        plt.axvline(x0, c="k", ls=":", alpha=0.4)
+        plt.show()
+
+    ### imag part
+    mask = -yp.imag > tol
+
+    idx_left = np.nonzero(mask)[0][0]
+    idx_right = np.nonzero(mask)[0][-1]
+
+    f_imag = np.empty_like(x)
+    mask = x < xp[idx_left]
+    f_imag[mask] = tangent_exp(xp, yp.imag, idx=idx_left)(x[mask])
+    mask = x > xp[idx_right]
+    f_imag[mask] = tangent_power_law(xp, yp.imag, idx=idx_right)(x[mask])
+    mask = np.logical_and(x >= xp[idx_left], x <= xp[idx_right])
     f_imag[mask] = checked_interp(
-        w[mask],
-        wp_sh,
-        fp.imag,
+        x[mask],
+        xp,
+        yp.imag,
         rtol=rtol,
         atol=atol,
         kind="cubic",
@@ -138,33 +202,41 @@ def interp_one_over_G_reta_w(w, wp, fp, wp_shift, rtol=1e-2, atol=1e-4, plot=Fal
         copy=False,
     )
 
-    f_imag[~mask] = 0.0
+    if plot:
+        plt.plot(xp, -yp.imag)
+        plt.xlim(*plt.xlim())  # freeze xlim
+        plt.semilogy()
+        tb.autoscale_y(logscale=True)
+
+        plt.plot(x, -f_imag, "--")
+
+        plt.axhline(tol, c="k", ls=":", alpha=0.4)
+        plt.axvline(xp[idx_left], c="k", ls=":", alpha=0.4)
+        plt.axvline(xp[idx_right], c="k", ls=":", alpha=0.4)
+
+        plt.show()
 
     f = f_real + 1j * f_imag
 
-    if plot:
-        plt.plot(w, f.imag)
-        plt.plot(wp_sh, fp.imag, "--")
-        plt.semilogy()
-        xcenter = (wp_sh[0] + wp_sh[-1]) / 2.0
-        xdev = np.abs(wp_sh[0] - wp_sh[-1])
-        plt.xlim(xcenter - xdev, xcenter + xdev)
-        tb.autoscale_y(logscale=True)
-        plt.show()
+    ### check norm
 
-        plt.axvline(wp_shift + wp[-1] / 10.0, c="k", ls=":")
-        plt.axvline(wp_shift - wp[-1] / 10.0, c="k", ls=":")
-        plt.plot(w, f.real - w)
-        plt.xlim(xcenter - xdev, xcenter + xdev)
-        tb.autoscale_y()
-        plt.plot(wp_sh, fp.real - wp_sh, "--")
-
-        plt.show()
-
-    norm_err = np.abs(-np.trapz(x=w, y=1.0 / f) - np.pi * 1j)
+    norm_err = np.abs(-integrate.simpson(x=x, y=f) - np.pi * 1j)
     if norm_err > 1e-1:
-        print(f"XXX Norm F_reta_w error = {norm_err}")
+        print(f"XXX Norm F_reta_w = {integrate.simpson(x=x, y=f)}")
     elif norm_err > 1e-2:
-        print(f"/!\ Norm F_reta_w error = {norm_err}")
+        print(f"/!\ Norm F_reta_w = {integrate.simpson(x=x, y=f)}")
+
+    ### plot inverse
+
+    if plot:
+        plt.plot(xp, np.imag(1.0 / yp))
+
+        plt.xlim(*plt.xlim())  # freeze xlim
+        plt.semilogy()
+        tb.autoscale_y(logscale=True)
+
+        plt.plot(x, np.imag(1.0 / f), "--")
+
+        plt.show()
 
     return f
