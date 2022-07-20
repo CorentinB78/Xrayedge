cb259916e386f3a850367fa8e0b799a4f9143ce4
Corentin Bertrand <cbertrand@flatironinstitute.org>
2022-07-20 18:42:02 -0400

    import QPC in __init__

--- xrayedge/__init__.py
+++ xrayedge/__init__.py
@@ -5,4 +5,4 @@ Solver for Hamiltonians of the type of the X-ray edge problem.
 __author__ = "Corentin Bertrand"
 
 from .solver import *
-from .reservoir import QuantumDot
+from .reservoir import QuantumDot, QPC
